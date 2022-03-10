62d417ea3b59f3f4b5f3431b49198c271030bdcc
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2022-03-10 14:53:03 +0100

    Update build.sh

--- build-all.sh
+++ build-all.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 warbudHome="C:/WEBSITES/Warbud"
-warbudRevitServer="$warbudHome/Warbud.Revit/Warbud.Revit.Server"
-cd $warbudHome/Warbud.Users/Warbud.Users.Api
-dotnet publish -c Release
-cd $warbudHome/Warbud.ReverseProxy/Warbud.ReverseProxy
-dotnet publish -c Release
-cd $warbudHome/Warbud.Mail
-dotnet publish -c Release
+warbudRevitServer="$warbudHome/Revit/Server"
+
+cd $warbudHome/Users
+sh build.sh
+
+cd $warbudHome/ReverseProxy
+sh build.sh
+
+cd $warbudHome/Mail
+sh build.sh
+
+cd $warbudHome/Revit/Naming
+sh build.sh
+
 cd $warbudRevitServer/Warbud.Revit.Update
-dotnet publish -c Release
+sh build.sh
+
 cd $warbudRevitServer/Warbud.Revit.Statistics
-dotnet publish -c Release
-cd $warbudRevitServer/Warbud.Revit.GQL/RevitRibbon.API
-dotnet publish -c Release
+dotnet publish -c Release -o $warbudHome/Builds/RevitStats
+
+cd $warbudRevitServer/Warbud.Revit.GQL
+dotnet publish -c Release -o $warbudHome/Builds/GQL
